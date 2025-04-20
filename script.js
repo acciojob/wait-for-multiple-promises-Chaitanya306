01ecef20d14780e3d25748defdbb796241a7b8dc
Chaitanya306 <98307330+Chaitanya306@users.noreply.github.com>
2025-04-20 11:21:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const output=document.getElementById('output')
 let p1=new Promise((res,rej)=>{ setTimeout(()=>res(Math.ceil(Math.random()*3)),1000) })
 let p2=new Promise((res,rej)=>{ setTimeout(()=>res(Math.ceil(Math.random()*3)),1000) })
 let p3=new Promise((res,rej)=>{ setTimeout(()=>res(Math.ceil(Math.random()*3)),1000) })
-output.innerHTML+=`<tr><td>Loading...</td></tr>`
+output.innerHTML+=`<tr id='loading'><td>Loading...</td></tr>`
 Promise.all([p1,p2,p3]).then((x) => {
 	output.innerHTML=''
 	output.innerHTML+=`<tr><td>Promise 1</td><td>${x[0]}</td></tr>`
